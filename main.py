c2b830a4c8121dd84494e554f98538fddcefb69a
EliasTec-uni <trinidadelias036@gmail.com>
2026-06-03 08:08:15 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+main.py — Punto de entrada del Sistema de Gestión de Estacionamiento
+Ejecutar: python main.py
+"""
+
+from parking_app import ParkingApp
+
+if __name__ == "__main__":
+    app = ParkingApp()
+    app.run()
